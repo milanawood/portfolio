12f9951dbff281658a74b1d07b04aa5731df1ac3
Milana Wood <mrsmilanawood@gmail.com>
2024-04-20 15:35:14 +0100

    space after thumb-over

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let overEl = null;
 
-const thumbEl = document.querySelector( ".thumb-over" );
+const thumbEl = document.querySelector( ".thumb-over " );
 
 const filenames = [ "bog.png", "gpe.png", "onewhale.png", "readingrainbow.png", "dtc.png" ];
 filenames.forEach( filename => { // preload images
@@ -10,8 +10,8 @@ filenames.forEach( filename => { // preload images
 
 window.addEventListener( "mousemove", e => {
     if ( !overEl ) return;
-    thumbEl.style.top = `${e.clientY}px`;
-    thumbEl.style.left = `${e.clientX}px`;
+    thumbEl.style.top = e.clientY + "px";
+    thumbEl.style.left = e.clientX + "px";
 } );
 
 document.querySelectorAll( ".project-link a" ).forEach( el => {
