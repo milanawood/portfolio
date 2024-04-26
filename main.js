809c5e15d1fb6a2d5031d54b847ffccdba92205f
Milana Wood <mrsmilanawood@gmail.com>
2024-04-26 20:19:59 +0100

    fix letter move bugs

--- main.js
+++ main.js
@@ -27,22 +27,38 @@ document.querySelectorAll( ".project-link a" ).forEach( el => {
 
 const movableText = document.getElementById('movableText');
 
+//get all the letter elements
+const letters = moveableText.querySelectorAll('.letter');
+
 // Function to handle touch move
 function handleTouchMove(event) {
   // Prevent default touch behavior (e.g., scrolling)
   event.preventDefault();
   
+  //loop through each letter and update its position
+  letters.forEach((letter, index) => {
+    const letTouchX = event.touches[index].clientX;
+    const letTouchY = event.touches[index].clientY;
+
+    //calculate new letter position
+    const newX = letTouchX - letter.offsetWidth / 2;
+    const newY = letTouchY - letter.offsetHeight / 2;
+
+    //set letter position
+    letter.style.left = newX + 'px';
+    letter.style.top = newY + 'px';
+  })
   // Get the touch position relative to the viewport
   const touchX = event.touches[0].clientX;
   const touchY = event.touches[0].clientY;
   
   // Calculate the new position of the text
-  const newX = touchX - movableText.offsetWidth / 2;
-  const newY = touchY - movableText.offsetHeight / 2;
+  const newMoveX = touchX - movableText.offsetWidth / 2;
+  const newMoveY = touchY - movableText.offsetHeight / 2;
   
   // Set the text position
-  movableText.style.left = newX + 'px';
-  movableText.style.top = newY + 'px';
+  movableText.style.left = newMoveX + 'px';
+  movableText.style.top = newMoveY + 'px';
 }
 
 // Add touch event listeners to the document
@@ -53,6 +69,11 @@ function handleTouchEnd() {
   // Reset the text position to its original state
   movableText.style.left = '50%';
   movableText.style.top = '50%';
+    // Reset the position of each letter to its original state
+    letters.forEach((letter) => {
+        letter.style.left = 'auto';
+        letter.style.top = 'auto';
+      });
 }
 
 // Add touchend event listener to the document
